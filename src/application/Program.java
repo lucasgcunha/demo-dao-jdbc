00284a587ebb7e67aaba61ea82e19c269391c9c2
Lucas Granier <lucgcunha@gmail.com>
2026-03-28 22:25:37 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -17,7 +18,7 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("=== Teste 2: seller findByDepartment ===");
+        System.out.println("\n=== Teste 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
 
@@ -25,11 +26,17 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("=== Teste 3: seller findAll ===");
+        System.out.println("\n=== Teste 3: seller findAll ===");
         list = sellerDao.findAll();
 
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== Teste 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
